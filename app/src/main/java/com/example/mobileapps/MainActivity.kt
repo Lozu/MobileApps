e530b5dfaeca2e7393ad382dbaafc4d66d09ea75
Volodymyr Azarov <ul0279990@edu.uni.lodz.pl>
2024-12-05 15:21:22 +0100

    Initial UI

---
+++ app/src/main/java/com/example/mobileapps/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mobileapps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+    }
+}
