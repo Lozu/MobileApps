00b7b372a36bf2d6058b01be93d47a67bd8f5612
Volodymyr Azarov <ul0279990@edu.uni.lodz.pl>
2024-12-08 13:40:05 +0100

    Basic navigation from one Activity to another
    Solves:
    	 Assignment #3: Onboarding navigation

--- app/src/main/java/com/example/mobileapps/LoginActivity.kt
+++ app/src/main/java/com/example/mobileapps/LoginActivity.kt
@@ -3,23 +3,21 @@ package com.example.mobileapps
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.textview.MaterialTextView
 
-class RegisterActivity : AppCompatActivity(R.layout.activity_register) {
-
+class LoginActivity : AppCompatActivity(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val login = findViewById<TextView>(R.id.register_footer_link)
+        val register = findViewById<TextView>(R.id.login_footer_link)
 
-        login.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+        register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
             finish()
         }
+
     }
 }
