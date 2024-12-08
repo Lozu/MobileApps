8e475c8420eb916a8a93b3a8f2b67d1d0dd858cd
Volodymyr Azarov <ul0279990@edu.uni.lodz.pl>
2024-12-08 11:28:23 +0100

    UI improvements
    Solves:
    	Assignment #1: Create 2 layouts from Figma

--- app/src/main/java/com/example/mobileapps/RegisterActivity.kt
+++ app/src/main/java/com/example/mobileapps/RegisterActivity.kt
@@ -2,13 +2,11 @@ package com.example.mobileapps
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
-class MainActivity : ComponentActivity() {
+class RegisterActivity : ComponentActivity(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
     }
 }
