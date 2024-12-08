e588faf7b2610dc723a7b2b0cdea55bfcbcf5c44
Volodymyr Azarov <ul0279990@edu.uni.lodz.pl>
2024-12-08 23:58:12 +0100

    Minor cleaning

--- app/src/main/java/com/example/mobileapps/SharedCredentialsManager.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mobileapps
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-
-class SharedCredentialsManager : ViewModel() {
-    val login_manager = MutableLiveData<CredentialsManager>()
-}
